5a43f39f94b3b5ffeb7baa24a1028a3cb8077ba9
Isuru sankhajith <isankhajith@gmail.com>
2024-08-27 18:09:29 +0530

    Modified code

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,29 +1,3 @@
-const products = [{
-    image:'images/products/intermediate-composite-basketball.jpg',
-    name:'Black and Gray Athletic Cotton Socks - 6 Pairs',
-    rating: {
-        stars: 4.5, 
-        count: 87
-    },
-    priceCents: 1090 
-},{
-    image:'images/ratings/rating-40.png',
-    name:'Intermediate Size Basketball',
-    rating: {
-        stars: 4, 
-        count: 127
-    },
-    priceCents: 2095
-},{
-    image:'images/products/adults-plain-cotton-tshirt-2-pack-teal.jpg',
-    name:'Adults Plain Cotton T-Shirt - 2 Pack',
-    rating: {
-        stars: 4.5, 
-        count: 56
-    },
-    priceCents: 799
-}];
-
 let productsHTML = '';
 products.forEach((product) => {
     productsHTML += `
@@ -39,7 +13,7 @@ products.forEach((product) => {
 
             <div class="product-rating-container">
                 <img class="product-rating-stars"
-                 src="images/ratings/rating- ${product.rating.stars * 10}.png">
+                 src="images/ratings/rating-${product.rating.stars * 10}.png">
                 <div class="product-rating-count link-primary">
                     ${product.rating.count} 
                 </div>
@@ -70,16 +44,47 @@ products.forEach((product) => {
                 <img src="images/icons/checkmark.png"> Added
             </div>
 
-            <button class="add-to-cart-button button-primary">
+            <button class="add-to-cart-button button-primary js-add-to-cart"
+            data-product-id="${product.id}">
                 Add to Cart
             </button>
         </div>
     `;
-    
 });
+// Assuming 'cart' is a globally accessible array
 
-console.log(productsHTML);
+document.querySelector('.js-products-grid').innerHTML = productsHTML;
 
-document.querySelector('.js-products-grid'). 
-innnerHTML = productsHTML;
+document.querySelectorAll('.js-add-to-cart').forEach((button) => {
+    button.addEventListener('click', () => {
+        const productId = button.dataset.productId; // Getting the product name from data attribute
+
+        let matchingItem;
+
+        cart.forEach((item ) =>{
+            if(productId === item.productId){
+
+            }
+        });
+
+        if(matchingItem){
+            matchingItem.quantity +=1;
+        }else{
+            cart.push({
+                productId: productId,
+                quantity: 1
+            }); 
+        }
+        //console.log(`${productName} added to cart`); // Optional: Log the action
+        console.log(cart); // Optional: Log the action
+    });
+});  
+
+
+
+
+
+
+
+
 
